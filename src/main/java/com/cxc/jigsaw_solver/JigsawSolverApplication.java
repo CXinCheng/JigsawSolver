b10d7d5e25d90f8e10ae92c8fd9c41e53e7fd527
xincheng-cai <xincheng_cai@apple.com>
2024-08-02 14:59:01 +0800

    chore: init

---
+++ src/main/java/com/cxc/jigsaw_solver/JigsawSolverApplication.java
@@ -0,0 +1,13 @@
+package com.cxc.jigsaw_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigsawSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JigsawSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cxc/jigsaw_solver/JigsawSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxc.jigsaw_solver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigsawSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
